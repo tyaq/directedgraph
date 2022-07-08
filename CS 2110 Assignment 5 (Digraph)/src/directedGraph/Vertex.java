eb5ea554bfff3685bec45a5d6154ecb4d44a0134
David Krell <92884243+DavidKrell@users.noreply.github.com>
2022-07-08 14:35:46 +0200

    Cleaner Code on Class Vertex.java
    Improved readability according to the official Java code conventions.

--- CS 2110 Assignment 5 (Digraph)/src/directedGraph/Vertex.java
+++ CS 2110 Assignment 5 (Digraph)/src/directedGraph/Vertex.java
@@ -4,32 +4,34 @@ import java.util.ArrayList;
 
 public class Vertex {
 
-	private String data;//Holds that actual data, does not need to be String Could have been generic
-	private ArrayList<Edge> edges;//Adjacency List
+	private String data;		// Holds that actual data, does not need to be String could have been generic
+	private ArrayList<Edge> edges;	// Adjacency List
 	
-	public Vertex(String d){
-		data=d;
+	public Vertex(String d) {
+		data = d;
 		edges = new ArrayList<Edge>();
-	}//End constructor
+	}
 	
-	public void add(Edge v){
-		if (edges != null && edges.contains(v) ) return;
+	public void add(Edge v) {
+		if (edges != null && edges.contains(v)) {
+			return;
+		}
 		edges.add(v);
-	}//End add method
+	}
 	
-	public void setData(String d){
-		data=d;
-	}//End setData
+	public void setData(String d) {
+		data = d;
+	}
 	
-	public String data(){
-		return data;
-	}//End getData
-	
-	public ArrayList<Edge> edges(){
-		return edges;
-	}//End getEdges
-	
-	public String toString(){
+	public String data() {
 		return data;
 	}
-}//End Vertex class
+	
+	public ArrayList<Edge> edges() {
+		return edges;
+	}
+	
+	public String toString() {
+		return data;
+	}
+}
