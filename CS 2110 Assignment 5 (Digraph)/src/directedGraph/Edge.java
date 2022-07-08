19ce2414cfe762257396db6cf3f8e26ffdc277d7
David Krell <92884243+DavidKrell@users.noreply.github.com>
2022-07-08 14:15:10 +0200

    Cleaner Code on Class Edge
    I mainly added JavaDoc Comments and improved readability by adding blank spaces.

--- CS 2110 Assignment 5 (Digraph)/src/directedGraph/Edge.java
+++ CS 2110 Assignment 5 (Digraph)/src/directedGraph/Edge.java
@@ -2,61 +2,98 @@ package directedGraph;
 
 public class Edge implements Comparable<Object>{
 
-	private int weight;//Edge Weight
-	private Vertex tail;//Start
-	private Vertex tip;//End
-	private Edge pair;//pair for undirected
+	private int weight;	//Edge Weight
+	private Vertex tail;	//Start
+	private Vertex tip;	//End
+	private Edge pair;	//pair for undirected
 	
-	/**
+	/** Constructor for class Edge
 	 * 
 	 * @param ta Tailing Vertex
 	 * @param ti Tipping Vertex
 	 * @param w Edge Weight
 	 */
-	public Edge(Vertex ta,Vertex ti,int w,Edge p){
+	public Edge(Vertex ta, Vertex ti, int w, Edge p) {
 		weight = w;
 		tail = ta;
 		tip = ti;
-		pair=p;
-		ta.add(this);//Tell the tail that it can follow this edge
-		//If you wanted undirected graph you could create an opposite edge and tell the tip about it.
-	}//End Constructor
+		pair = p;
+		ta.add(this);	//Tell the tail that it can follow this edge
+		
+		/* If you wanted undirected graph you could create an opposite edge and tell the tip about it. */
+	}
 	
+	/**
+	 * Compares the weight of edges
+	 *
+	 * @return 0 if the edges weight is equal, 1 if less, and -1 if bigger
+	 */
 	public int compareTo(Object o) {
-        Edge e1 = (Edge)o;
-        if(e1.weight==this.weight)
-            return 0;
-        return e1.weight < this.weight ? 1 : -1;
-    }//End Compare
+        	Edge e1 = (Edge) o;
+        	if(e1.weight == this.weight) {
+            		return 0;
+		}
+        	return e1.weight < this.weight ? 1 : -1;
+    	}
 	
-	public void setWeight(int w){
-		weight=w;
-	}//End setWeight
-	
-	public int weight(){
+	/**
+	 * Setter-Method to set the weight.
+	 */
+	public void setWeight(int w) {
+		weight = w;
+	}
+	/**
+	 * Getter-Method for the weight.
+	 *
+	 * @return Returns the weight.
+	 */
+	public int weight() {
 		return weight;
-	}//End getWeight
+	}
 	
-	public Vertex tip(){
+	/**
+	 * Getter-Method for the tip of an edge
+	 *
+	 * @return Returns the tip vertex of an edge.
+	 */
+	public Vertex tip() {
 		return tip;
 	}
-	
-	public Vertex tail(){
+	/**
+	 * Getter-Method for the tail of an edge
+	 *
+	 * @return Returns the tail vertex of an edge.
+	 */
+	public Vertex tail() {
 		return tail;
 	}
-	
-	public Edge pair(){
+	/**
+	 * Getter-Method for the edge pair
+	 *
+	 * @return Returns the edge
+	 */
+	public Edge pair() {
 		return pair;
-	}//end getPair
-	
-	public Edge find(Vertex ta){
-		if (tail==ta) {
-			return this;
-		}//end if
-		return null;
-		
 	}
-	public String toString(){
-		return "["+tail+"->"+tip+" Weight:"+weight+"]";
+	
+	/*
+	 * Checks if the edge has a specific tail.
+	 *
+	 * @param ta The tail vertex that should be compared to
+	 * @return returns the edge if the tails are equal, returns null if not
+	 */
+	public Edge find(Vertex ta) {
+		if (tail == ta) {
+			return this;
+		}
+		return null;	
+	}
+	/*
+	 * Converts the edge into a String.
+	 *
+	 * @return A String representing the Edge.
+	 */
+	public String toString() {
+		return "[" + tail + "->" + tip + " Weight:" + weight + "]";
 	}
 }
